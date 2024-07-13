0b81253ecefaf7b9c819131eaebc9ebf2e6623e7
Truongle68 <truonglephuc11@gmail.com>
2024-07-13 22:20:11 +0700

    edit homepage(card)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getAllStudent } from "../services/studentService";
 import NavbarHeader from "./Navbar";
-import { Container } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
@@ -30,12 +30,27 @@ const Home = () => {
       <Container>
         <h2 className="title">STUDENT LIST</h2>
         <div className="white-board">
-          {listStudent!==null && listStudent.length>0 &&
-          listStudent.map((student)=>{
-            return(
-              <>
-              
-              <div className="info-container" key={student.id}>
+          {listStudent !== null && listStudent.length > 0 &&
+            listStudent.map((student) => {
+              return (
+                <>
+                  <Card style={{ width: '18rem', marginBottom: '10px' }}>
+                    <Card.Img width={286} height={190} variant="top" src={student.image} />
+                    <Card.Body>
+                      {/* <Card.Title>Card Title</Card.Title> */}
+                      <Card.Text>
+                        Id: <Link style={{ textDecoration: "none" }} to={`/detail/${student.id}`}>{student.id}</Link>
+                      </Card.Text>
+                      <Card.Text>Name: {student.name}</Card.Text>
+                      <Card.Text>Date of birth: {student.dateofbirth}</Card.Text>
+                      <Card.Text>Gender: {`${student.gender}` === "true" ? "male" : "female"}</Card.Text>
+                      <Card.Text>Class: {student.class}</Card.Text>
+                      <Card.Text>Feedback: {student.feedback}</Card.Text>
+                      <Button className="detail-btn" variant="outline-primary" onClick={() => navigate(`/detail/${student.id}`)}>Detail</Button>
+                    </Card.Body>
+                  </Card>
+
+                  {/* <div className="info-container" key={student.id}>
                 <div className="student-img">
                   <img src={student.image}/>
                 </div>
@@ -52,10 +67,10 @@ const Home = () => {
                   <Button className="detail-btn" variant="outline-primary" onClick={() => navigate(`/detail/${student.id}`)}>Detail</Button>
 
                 </div>
-              </div>
-              </>
-            )
-          })}
+              </div> */}
+                </>
+              )
+            })}
         </div>
 
       </Container>
